ffc853d59e4e537040a984833aac9e29e1469d83
Venkateshwara <venkateshwar.gopisetti@gmail.com>
2021-10-14 18:13:00 +0530

    --no-edit

--- update_repositories.bat
+++ update_repositories.bat
@@ -3,9 +3,10 @@ FOR /F %%G in ('dir /b/ad') DO (call :subroutine "%%G")
 GOTO :eof
 
 :subroutine
- set "wdir=%~1"
- echo Current Repository - %wdir%
- cd %wdir%
- git push --all origin
- cd ..
- GOTO :eof
+set "wdir=%~1"
+echo Current Repository - %wdir%
+cd %wdir%
+git add . && git commit -am --no-edit
+git push --all origin
+cd ..
+GOTO :eof
